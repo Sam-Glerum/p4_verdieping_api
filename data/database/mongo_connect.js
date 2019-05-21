3a1d64f69c0d0f7e15965912983a29f987ffd424
Sam Glerum <samuelglerum@hotmail.com>
2019-05-21 11:27:33 +0200

    fixed unhandled promise rejection in mongo connection

--- data/database/mongo_connect.js
+++ data/database/mongo_connect.js
@@ -11,7 +11,11 @@ module.exports = class mongoConnect {
             url = "mongodb://127.0.0.1:27017/" + DATABASE_NAME;
         }
         // Establish a connection with the Mongo database
-        mongoose.connect(url, {useNewUrlParser: true});
+        mongoose.connect(url, {useNewUrlParser: true})
+            .catch(() => {
+                console.log('Could not connect to mongo db');
+            });
+
         // Print a message to show the connection has been established
         mongoose.connection.once('open', () => {
             console.log('Connected to the mongo database ' + DATABASE_NAME)
